6e63df3df28aa168f3054f49f4b253494d24989b
Piotr Baltyn <kontakt@clthub.com.pl>
2025-08-18 23:41:34 +0200

    build(config): replace next.config.ts with working next.config.js (next-intl)

---
+++ next.config.js
@@ -0,0 +1,16 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@opentelemetry/instrumentation': 'commonjs @opentelemetry/instrumentation',
+      });
+    }
+    return config;
+  },
+};
+
+const withNextIntl = require('next-intl/plugin')('./src/i18n/request.ts');
+module.exports = withNextIntl(nextConfig);
