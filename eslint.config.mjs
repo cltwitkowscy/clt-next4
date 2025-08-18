77ae79957d24d995275e411cd16a0415c9bad401
CLTHUB <cltplhub@gmail.com>
2025-08-18 10:33:28 +0200

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,12 @@
-﻿export default [
-  { ignores: ['.netlify/**', 'netlify.toml', '.next', 'node_modules'] },
+﻿import nextPlugin from '@next/eslint-plugin-next';
+
+export default [
+  { ignores: ['.next/**', 'node_modules/**', '.netlify/**', 'netlify.toml'] },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: { '@next/next': nextPlugin },
+    rules: {
+      ...nextPlugin.configs['core-web-vitals'].rules
+    }
+  }
 ];
