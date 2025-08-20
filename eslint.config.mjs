2dda1ea1c09f2b1a08daf334cda6f3910659e4d8
Piotr Baltyn <kontakt@clthub.com.pl>
2025-08-20 20:14:59 +0200

    intl autopatch v2: prefix Counter/CounterForm/BaseTemplate, reliable tsc/build

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+﻿import next from "eslint-config-next";
+export default [
+  ...next(),
+  { rules: {
+      "react-dom/no-unsafe-target-blank": "off",
+      "@typescript-eslint/consistent-type-definitions": "off"
+    }
+  }
+];
